46d45cce8c48712d8aed13aff13e1b64d2487783
Matthew Tavendale <TntMatthew@users.noreply.github.com>
2018-01-19 19:52:44 +0000

    Initial commit, v0.1

---
+++ distrandomiser.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+import distance, random, sys, os, collections
+
+# Change to true if you want debug output (this will be a command line
+# flag eventually)
+debug = False
+
+version = '0.1-alpha'
+
+def debug_print(text):
+    if debug:
+        print(text)
+
+# Get the directory passed
+try:
+    leveldir = sys.argv[1] + '/'
+except IndexError:
+    # Use the current working directory
+    leveldir = ''
+
+# Level name shorthands, for convenience
+bs = f'{leveldir}broken symmetry.bytes'
+ls = f'{leveldir}lost society.bytes'
+ns = f'{leveldir}negative space.bytes'
+de = f'{leveldir}departure.bytes'
+gz = f'{leveldir}ground zero.bytes'
+af = f'{leveldir}aftermath.bytes'
+fr = f'{leveldir}friction.bytes'
+ma = f'{leveldir}the thing about machines.bytes'
+co = f'{leveldir}corruption.bytes'
+mo = f'{leveldir}monolith.bytes'
+du = f'{leveldir}destination unknown.bytes'
+cr = f'{leveldir}credits.bytes'
+
+# Get the location of the Distance directory
+if sys.platform == 'linux' or sys.platform == 'linux2':
+    distdir = os.path.expanduser('~/.config/refract/Distance')
+elif sys.platform == 'darwin':
+    distdir = os.path.expanduser('~/Library/Application Support/Refract/Distance')
+elif sys.platform == 'windows':
+    distdir = os.path.expanduser('~/My Games/Distance')
+
+# This requires some explanation. It seems the distance module doesn't have
+# setters on the ability settings on the enable triggers, but you CAN
+# alter the transform. So... here I have all the transforms for each
+# level's ability box, and obtain every ability box in advance so I
+# can alter the abilities enabled on each level.
+#
+# ...This is the most ridiculous hack I think I've ever written.
+# Desperate times call for desperate measures, indeed... at least
+# it works?
+bs_abox_transform = ((691.8410034179688, 7.955999851226807, -2433.93310546875), (0, 0, 0, 1), (66.25, 66.25, 362.3999938964844))
+ls_abox_transform = ((-685.8243408203125, 26.670000076293945, -389.71746826171875), (0.0, 0.5000000596046448, 0.0, -0.8660253882408142), (112.78399658203125, 112.78399658203125, 484.0814208984375))
+de_abox_trasnform = ((-3988.756103515625, -29.958999633789062, -2159.41552734375), (0, 0, 0, 1), (200.0, 200.0, 638.0))
+af_abox_transform = ((383.33331298828125, 164.0807647705078, -6622.5146484375), (0.3420201539993286, 0.9396926760673523, -1.719538545330579e-06, -9.84505049927975e-07), (199.99996948242188, 200.0, 529.7343139648438))
+mo_abox_transform = ((-211.4759979248047, 116.73799896240234, -5069.0400390625), (0, 0, 0, 1), (268.1130065917969, 268.1130065917969, 268.1130065917969))
+
+# Get the ability boxes
+for lvl in [ls, de, af]:
+    lvlbytes = distance.Level(lvl)
+    for obj in lvlbytes.layers[0].objects:
+        if obj.type == 'EnableAbilitiesBox':
+            if obj.abilities['EnableJumping'] == 1:
+                jump_abox = obj
+            elif obj.abilities['EnableFlying'] == 1:
+                wings_abox = obj
+            elif obj.abilities['EnableJetRotating'] == 1:
+                jets_abox = obj
+
+aboxes = [jump_abox, wings_abox, jets_abox]
+
+
+#available_levels = [bs, ls, ns, de, gz, af, fr, ma, co, mo, du, cr]
+available_levels = [bs, ls, ns, de, gz, af, fr, ma, co, mo]
+available_abilities = ['EnableJumping', 'EnableJetRotating',
+                       'EnableFlying']
+
+# This is used to keep track of the levels currently in the playlist
+tracked_levels = []
+
+# Same, but abilities
+tracked_abilities = []
+
+# Keep track of what abilities the player has
+boost_enabled = True
+jump_enabled = False
+wings_enabled = False
+jets_enabled = False
+
+print(f'Distrandomiser Version {version}\n')
+
+# 1 = Standard
+# 1337 = No softlock checking, essentially a test mode
+mode = 1
+
+if mode == 1:
+    #requires_jets = [fr, ma, co]
+    requires_jets = []
+    requires_jump = [ns, fr, af, ma, mo, du]
+    requires_boost = [de, gz, af]
+    # These can be done with jets as well.
+    requires_wings = [fr, ma, gz, du, mo, af]
+elif mode == 1337:
+    requires_jets = []
+    requires_jump = []
+    requires_boost = []
+    requires_wings = []
+
+
+seed = input('Seed (type nothing for random seed): ')
+
+if seed == '':
+    seed = None
+    print('Generating randomiser game with random seed...')
+else:
+    print(f'Generating randomiser game with seed {seed}...')
+
+random.seed(seed)
+
+ability_order = []
+
+while len(ability_order) < 3:
+    if len(ability_order) == 0:
+        selectint = random.randint(0,2)
+    elif len(ability_order) == 1:
+        selectint = random.randint(0,1)
+    elif len(ability_order) == 2:
+        selectint = 0
+
+    ability_order.append(available_abilities[selectint])
+    available_abilities.remove(available_abilities[selectint])
+
+ability_trigger_count = 0
+
+debug_print(ability_order)
+
+#while len(tracked_levels) != 12:
+while len(tracked_levels) != 10:
+    level = available_levels[random.randint(0,len(available_levels) - 1)]
+
+    if level not in tracked_levels:
+
+        debug_print(level)
+        if level in requires_jump and not jump_enabled:
+            debug_print('needs jump!')
+            continue
+        if level in requires_wings:
+            if not wings_enabled and not jets_enabled:
+                debug_print('needs rotation!')
+                continue
+
+        lvlbytes = distance.Level(level)
+        lvlbytes.settings.name = '???'
+        lvlbytes.settings.modes = collections.OrderedDict([(5,0), (13,0), (1,0), (2,0), (8,0)])
+        lvlbytes.settings.abilities = (0,
+                                       int(not wings_enabled),
+                                       int(not jump_enabled),
+                                       int(not boost_enabled),
+                                       int(not jets_enabled)
+        )
+
+        unwanted_objects = ['AdventureAbilitySettings', 'WingCorruptionZone',
+                            'WingCorruptionZoneLarge', 'InfoDisplayBox',
+                            'InfoAndIndicatorDisplayBox']
+        objects = [obj for obj in lvlbytes.layers[0].objects if obj.type not in unwanted_objects];
+
+        origabox = next((obj for obj in objects if obj.type == 'EnableAbilitiesBox'), None)
+        if origabox != None:
+            if len(tracked_abilities) < 3:
+                ability = ability_order[len(tracked_abilities)]
+                if ability == 'EnableJumping':
+                    if level == de:
+                        if not jets_enabled and not wings_enabled:
+                            print('level can\'t give jump; it needs flight')
+                            continue
+                        else:
+                            abox = jump_abox
+                    else:
+                        abox = jump_abox
+                elif ability == 'EnableFlying':
+                    abox = wings_abox
+                elif ability == 'EnableJetRotating':
+                    abox = jets_abox
+                tracked_abilities.append(ability)
+            else:
+                abox = aboxes[random.randint(0,2)]
+
+            if abox == jump_abox:
+                jump_enabled = True
+                debug_print('jump on')
+            elif abox == wings_abox:
+                wings_enabled = True
+                debug_print('wings on')
+            elif abox == jets_abox:
+                jets_enabled = True
+                debug_print('jets on')
+
+            abox.transform = origabox.transform
+            objects.remove(origabox)
+            objects.append(abox)
+
+
+        debug_print('added ' + level)
+        tracked_levels.append(level)
+        available_levels.remove(level)
+        #print(tracked_levels)
+
+
+        lvlbytes.layers[0].objects = objects
+
+        # Write the modified data
+        print(f'Writing randomiser{len(tracked_levels)}.bytes...')
+        lvlbytes.write(f'{distdir}/Levels/MyLevels/randomiser{len(tracked_levels)}.bytes')
+
+debug_print(tracked_levels)
+
+playlisttext = '<GameObject Name="LevelPlaylist" GUID="0">\n' + \
+                    '<Transform Version="0" GUID="0" />\n' + \
+                    '<LevelPlaylist Version="0" GUID="0">\n' + \
+                    '<PlaylistName>Randomiser</PlaylistName>\n' + \
+                    f'<NumberOfLevelsInPlaylist>{len(tracked_levels)}</NumberOfLevelsInPlaylist>\n' + \
+                    '<ModeAndLevelInfoVersion>0</ModeAndLevelInfoVersion>\n'
+
+runs = 0
+for level in tracked_levels:
+    runs += 1
+    playlisttext += '<GameMode>9</GameMode>\n' + \
+                    '<LevelName>???</LevelName>\n' + \
+                    f'<LevelPath>MyLevels/randomiser{runs}.bytes</LevelPath>'
+
+playlisttext += '</LevelPlaylist>\n</GameObject>'
+
+print(f'Writing randomiser.xml...')
+with open(f'{distdir}/LevelPlaylists/randomiser.xml', 'w') as playlistfile:
+    playlistfile.write(playlisttext)
---
+++ readme.md
@@ -0,0 +1,73 @@
+# distrandomiser - Distance, but Randomised (Alpha)
+This script takes in the Distance Adventure mode maps, and spits out
+a playlist with both the map and ability unlock order randomised -
+abilities are even adjusted so that they logically carry over like they
+would if the maps were actually played in that order. This can be used
+when racing the game, to make it just a little bit more interesting for
+those who have played the game to death.
+
+Absolutely nobody asked for this, but guess what? I made it anyway.
+
+As you can probably expect, this is inspired by various game randomisers
+such as [ALTTP Randomiser](http://vt.alttp.run). You're probably wondering
+why I decided to adapt an action-adventure game mode into a much more
+linear game, and I can't actually answer that - I don't know. But it
+exists!
+
+This is currently in alpha. Most of the core functionality is there
+and works reasonbly well, however it may need tweaking, the code could
+be cleaner, and there's obviously more features I want to add. Plus,
+Distance itself is currently in beta - this will need to be updated to
+support the finished Adventure mode once it is completed, so it doesn't
+really make sense to call this a full, stable release until that happens.
+
+As of right now, there's no real options so to speak, besides seed.
+I plan to have more options available in the future, as well as the
+ability to generate customised games, not just randomised ones.
+
+It shouldn't be possible to get stuck with the current algorithm, but
+you may need to use techs like wallshoving (pushing yourself against
+walls to leave the track), which may be tricky or unpredictable. Again,
+alpha; this is still somewhat experimental. If you do get stuck and
+can't progress, make a new issue so I can check that out (or tell you
+how to beat it if you just have no idea what to do).
+
+## Differences from the normal game
+- Obviously, as mentioned in the first sentence of this readme, the order
+  of the maps and the abilities unlocked have been randomised.
+- Boost is enabled by default, you do not need to pick it up at an
+  ability trigger.
+- Wing corruption zones have been removed to allow for more variety.
+- All ability tutorial text triggers have been removed, because you
+  don't need to learn how to play the game when you're doing randomiser.
+
+## How to Use
+First, you need to aquire the randomiser script, and install the
+dependencies required to use it:
+
+    $ git clone https://github.com/TntMatthew/distrandomiser.git
+    $ pip install distance numpy numpy-quaternion
+    
+After that, you need to get the .bytes files of the original Adventure
+maps. You can do this for most of them by simply opening them in the
+editor and saving them as a new file. Don't worry about getting a hold
+of Destination Unknown or the Credits level for now - neither are
+included at the moment as I can't be bothered to install Spectrum
+to get them - and even then, once they are implemented, they may only be
+inserted completely unmodified at the end of the playlist, as nobody wants
+a long unskippable cutscene in the middle of a run.
+
+Once you have the map files, you can run the randomiser script
+with Python.
+
+    $ python distrandomiser.py [dir_of_level_files]
+
+When you run it, you will be asked to input a seed. You can simply just
+press enter if you want to use a random seed. The script will then
+take in the map files, edit them, then automatically spit them out
+into your MyLevels directory with the filenames `randomiser1` through
+`randomiser10`. (Don't worry, all mode tags are stripped so they won't
+clog up your my levels list ingame.) A playlist file, `randomiser.xml`,
+will also be generated and automatically put into the LevelPlaylists
+directory. When you want to play your randomised game, go to any Arcade
+level select, open the playlist list, and select randomiser.
